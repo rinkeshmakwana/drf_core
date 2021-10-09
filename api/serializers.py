2b44391c8bf047d4048aad8baa08e161f483a6b1
rinkesh.d.makwana <rinkesh.d.makwana@doyenhub.com>
2021-10-09 14:36:17 +0530

    deserialization concept and create data

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,11 @@
 from rest_framework import serializers
+from .models import Employee
 
 
 class EmployeeSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=100)
     employee_code = serializers.IntegerField()
     department = serializers.CharField(max_length=50)
+
+    def create(self, validate_data):
+        return Employee.objects.create(**validate_data)
--- api/views.py
+++ api/views.py
@@ -1,4 +1,8 @@
+import io
+
 from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
 
 from .models import Employee
@@ -11,12 +15,12 @@ def employee_list(request):
     """
     emp = Employee.objects.all()
     # < QuerySet[ < Employee: Employee object(1) >] >
-    serializer = EmployeeSerializer(emp, many=True)
+    serializer = EmployeeSerializer(emp, many=True)     # model instance into Python native datatype
     # serializer.data : [OrderedDict([('name', 'Rinkesh'), ('employee_code', 1), ('department', 'Developer')]),
     #                     OrderedDict([('name', 'Nikunj'), ('employee_code', 2), ('department', 'Developer')])]
 
     # if using HttpResponse use this code
-    # json_data = JSONRenderer().render(serializer.data)
+    # json_data = JSONRenderer().render(serializer.data)     # serialization : Python native datatype to json data
     # # b'[{"name":"Rinkesh","employee_code":1,"department":"Developer"}]'
     # return HttpResponse(json_data, content_type="application/json")
 
@@ -30,13 +34,38 @@ def employee_detail(request, pk):
     """
     emp = Employee.objects.get(id=pk)
     # Employee object(1)
-    serializer = EmployeeSerializer(emp)
+    serializer = EmployeeSerializer(emp)     # model instance into Python native datatype
     # serializer.data : {'name': 'Rinkesh', 'employee_code': 1, 'department': 'Developer'}
 
     # if using HttpResponse use this code
-    # json_data = JSONRenderer().render(serializer.data)
+    # json_data = JSONRenderer().render(serializer.data)     # serialization : Python native datatype to json data
     # b'{"name":"Rinkesh","employee_code":1,"department":"Developer"}'
     # return HttpResponse(json_data, content_type="application/json")
 
     # if using JsonResponse use this code
     return JsonResponse(serializer.data)
+
+
+@csrf_exempt
+def employee_create(request):
+    """
+    simplest create django function based view that returns json created data
+    """
+    if request.method == "POST":
+        json_data = request.body
+        # b'{"name": "Rahul", "employee_code": "4", "department": "Developer"}'
+
+        # deserialization json data to python native datatype
+        stream = io.BytesIO(json_data)
+        python_data = JSONParser().parse(stream)
+        # {'name': 'Rahul', 'employee_code': '4', 'department': 'Developer'}
+
+        serializer = EmployeeSerializer(data=python_data)
+
+        if serializer.is_valid():
+            serializer.save()           # calls serializer update method if instance exist else create method
+            response = {'message': "Data Created"}
+            json_data = JSONRenderer().render(response)
+            return HttpResponse(json_data, content_type="application/json")
+        json_data = JSONRenderer().render(serializer.errors)
+        return HttpResponse(json_data, content_type="application/json")
