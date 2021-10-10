222a64be7b916e0811940c28bc040d767c8c0ed2
rinkesh.d.makwana <rinkesh.d.makwana@doyenhub.com>
2021-10-10 14:58:20 +0530

    model serializer concept implemented convert normal serializer to model serializer

--- api/serializers.py
+++ api/serializers.py
@@ -3,35 +3,44 @@ from rest_framework import serializers
 from .models import Employee
 
 
-def start_with_r(value):
-    if value[0].lower() != 'r':
-        raise serializers.ValidationError("Name should be start with r")
+# class EmployeeSerializer(serializers.Serializer):
+#     """custom serializer"""
+#
+#     def start_with_r(value):
+#         if value[0].lower() != 'r':
+#             raise serializers.ValidationError("Name should be start with r")
+#
+#     name = serializers.CharField(max_length=100, validators=[start_with_r])     # validator as custom function
+#     employee_code = serializers.IntegerField()
+#     department = serializers.CharField(max_length=50)
+#
+#     def create(self, validate_data):
+#         return Employee.objects.create(**validate_data)
+#
+#     def update(self, instance, validated_data):
+#         instance.name = validated_data.get('name', instance.name)
+#         instance.employee_code = validated_data.get('employee_code', instance.employee_code)
+#         instance.department = validated_data.get('department', instance.department)
+#         instance.save()
+#         return instance
+#
+#     # field level validation
+#     def validate_employee_code(self, value):
+#         if value >= 200:
+#             raise serializers.ValidationError("Employees quota over")
+#
+#     # object level validation
+#     def validate(self, data):
+#         name = data.get('name')
+#         department = data.get('department')
+#         if name.lower() == 'rinkesh' and department.lower() != 'developer':
+#             raise ValidationError("Department must be developer")
+#         return data
 
 
-class EmployeeSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=100, validators=[start_with_r])     # validator as custom function
-    employee_code = serializers.IntegerField()
-    department = serializers.CharField(max_length=50)
+class EmployeeSerializer(serializers.ModelSerializer):
+    """model serializer"""
 
-    def create(self, validate_data):
-        return Employee.objects.create(**validate_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.employee_code = validated_data.get('employee_code', instance.employee_code)
-        instance.department = validated_data.get('department', instance.department)
-        instance.save()
-        return instance
-
-    # field level validation
-    def validate_employee_code(self, value):
-        if value >= 200:
-            raise serializers.ValidationError("Employees quota over")
-
-    # object level validation
-    def validate(self, data):
-        name = data.get('name')
-        department = data.get('department')
-        if name.lower() == 'rinkesh' and department.lower() != 'developer':
-            raise ValidationError("Department must be developer")
-        return data
+    class Meta:
+        model = Employee
+        fields = ['id', 'name', 'employee_code', 'department']
